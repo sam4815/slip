432bd88332eca890e082c0fdde58315aed5e3391
sam <sam.brougham@gmail.com>
2022-05-14 19:37:41 -0400

    Integrate with mpc library

--- slip.c
+++ slip.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpc/mpc.h"
 
 #ifdef _WIN32
 #include <string.h>
@@ -20,19 +21,42 @@ void add_history(char* unused) {}
 #else
 #include <editline/readline.h>
 #endif
-
-
+ 
 int main(int argc, char** argv) {
+	mpc_parser_t* Slip = mpc_new("slip");
+	mpc_parser_t* Expression = mpc_new("expression");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Number = mpc_new("number");
+
+	mpca_lang(MPCA_LANG_DEFAULT,
+		"									\
+			operator   : '+' | '-' | '*' | '/' ;				\
+			number     : /-?[0-9]+/ ;					\
+			expression : <number> | '(' <operator> <expression>+ ')' ;	\
+			slip	   : /^/ <operator> <expression>+ /$/ ;	  		\
+		",
+	Slip, Expression, Operator, Number);
 	puts("slip v0.1\n");
 
 	while (1) {
 		char* input = readline("slip> ");
 		add_history(input);
+
+		mpc_result_t r;
+
+		if (mpc_parse("<stdin>", input, Slip, &r)) {
+			mpc_ast_print(r.output);
+			mpc_ast_delete(r.output);
+		} else {
+			mpc_err_print(r.error);
+			mpc_err_delete(r.error);
+		}
 		
-		printf("No you're a %s\n", input);
+		printf("No u a %s\n", input);
 		free(input);
 	}
 
+	mpc_cleanup(4, Slip, Expression, Operator, Number);
 	return 0;
 }
 
