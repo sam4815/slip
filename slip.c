fa197e50df4d135daf4ab5970a428390d0526b31
sam <sam.brougham@gmail.com>
2022-05-15 19:50:58 -0400

    Evaluate numeric expressions

--- slip.c
+++ slip.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
 #include "mpc/mpc.h"
 
 #ifdef _WIN32
@@ -21,7 +23,37 @@ void add_history(char* unused) {}
 #else
 #include <editline/readline.h>
 #endif
- 
+
+long operate(char* operation, long x, long y) {
+	if (strcmp(operation, "+") == 0) { return x + y; }
+	if (strcmp(operation, "-") == 0) { return x - y; }
+	if (strcmp(operation, "/") == 0) { return x / y; }
+	if (strcmp(operation, "*") == 0) { return x * y; }
+	if (strcmp(operation, "%") == 0) { return x % y; }
+	if (strcmp(operation, "^") == 0) { return pow(x, y); }
+
+	if (strcmp(operation, "max") == 0) { return x > y ? x : y; } 
+	if (strcmp(operation, "min") == 0) { return x < y ? x : y; } 
+
+	return 0;
+}
+
+long parse_tree(mpc_ast_t* tree) {
+	bool is_number = strstr(tree->tag, "number") != NULL;	
+	if (is_number) {
+		return(atoi(tree->contents));
+	}
+
+	long accumulator = parse_tree(tree->children[2]);
+
+	for (int i = 3; i < tree->children_num - 1; i++) {
+		long child_value = parse_tree(tree->children[i]);
+		accumulator = operate(tree->children[1]->contents, accumulator, child_value);
+	}
+
+	return accumulator;
+}
+
 int main(int argc, char** argv) {
 	mpc_parser_t* Slip = mpc_new("slip");
 	mpc_parser_t* Expression = mpc_new("expression");
@@ -29,34 +61,35 @@ int main(int argc, char** argv) {
 	mpc_parser_t* Number = mpc_new("number");
 
 	mpca_lang(MPCA_LANG_DEFAULT,
-		"									\
-			operator   : '+' | '-' | '*' | '/' ;				\
-			number     : /-?[0-9]+/ ;					\
-			expression : <number> | '(' <operator> <expression>+ ')' ;	\
-			slip	   : /^/ <operator> <expression>+ /$/ ;	  		\
-		",
-	Slip, Expression, Operator, Number);
+			"                                                                       \
+			operator   : '+' | '-' | '*' | '/' | '%' | '^'                          \
+			           | \"min\" | \"max\" ;                                        \
+			number     : /-?[0-9]+/ ;                                               \
+			expression : <number> | '(' <operator> <expression>+ ')' ;              \
+			slip	   : /^/ <operator> <expression>+ /$/ ;                         \
+			",
+			Slip, Expression, Operator, Number);
+
 	puts("slip v0.1\n");
 
 	while (1) {
 		char* input = readline("slip> ");
 		add_history(input);
 
-		mpc_result_t r;
+		mpc_result_t result;
+		mpc_parse("<stdin>", input, Slip, &result);
 
-		if (mpc_parse("<stdin>", input, Slip, &r)) {
-			mpc_ast_print(r.output);
-			mpc_ast_delete(r.output);
-		} else {
-			mpc_err_print(r.error);
-			mpc_err_delete(r.error);
-		}
-		
-		printf("No u a %s\n", input);
+		if (getenv("PRINT_TREE")) { mpc_ast_print(result.output); }
+
+		long output = parse_tree(result.output);
+		printf("%ld\n", output);
+
 		free(input);
+		mpc_ast_delete(result.output);
 	}
 
 	mpc_cleanup(4, Slip, Expression, Operator, Number);
+	
 	return 0;
 }
 
