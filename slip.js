1ef27264f607f18a1f957faffc28b5f81dca0312
sam4815 <32017929+sam4815@users.noreply.github.com>
2022-06-18 19:40:06 +0000

    Deploying to gh-pages from @ sam4815/slip@213c7729f370355e5add7e063d6be9dea1bf5d4b 🚀

--- slip.js
+++ slip.js
@@ -669,8 +669,8 @@ var wasmMemory;
 // In the wasm backend, we polyfill the WebAssembly object,
 // so this creates a (non-native-wasm) table for us.
 var wasmTable = new WebAssembly.Table({
-  'initial': 80,
-  'maximum': 80 + 0,
+  'initial': 78,
+  'maximum': 78 + 0,
   'element': 'anyfunc'
 });
 
@@ -1289,11 +1289,11 @@ function updateGlobalBufferAndViews(buf) {
 }
 
 var STATIC_BASE = 1024,
-    STACK_BASE = 5250608,
+    STACK_BASE = 5250880,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 7728,
-    DYNAMIC_BASE = 5250608,
-    DYNAMICTOP_PTR = 7568;
+    STACK_MAX = 8000,
+    DYNAMIC_BASE = 5250880,
+    DYNAMICTOP_PTR = 7840;
 
 assert(STACK_BASE % 16 === 0, 'stack must start aligned');
 assert(DYNAMIC_BASE % 16 === 0, 'heap must start aligned');
@@ -1872,7 +1872,7 @@ var ASM_CONSTS = {
 
 
 
-// STATICTOP = STATIC_BASE + 6704;
+// STATICTOP = STATIC_BASE + 6976;
 /* global initializers */  __ATINIT__.push({ func: function() { ___wasm_call_ctors() } });
 
 
@@ -1929,7 +1929,7 @@ var ASM_CONSTS = {
     }
 
   function _emscripten_get_sbrk_ptr() {
-      return 7568;
+      return 7840;
     }
 
   function _emscripten_memcpy_big(dest, src, num) {
